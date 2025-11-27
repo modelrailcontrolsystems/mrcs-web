7297ba7aac5efe78d9f5cee7aea2c69fba6dcf65
Bruno Beloff <bruno.beloff@southcoastscience.com>
2025-11-27 17:53:49 +0000

    mrcs-web initial commit

---
+++ src/mrcs_web/app/routers/mlg.py
@@ -0,0 +1,10 @@
+"""
+Created on 27 Nov 2025
+
+@author: Bruno Beloff (bbeloff@me.com)
+
+Message logger (MLG) API
+
+https://fastapi.tiangolo.com/tutorial/bigger-applications/#an-example-file-structure
+"""
+
