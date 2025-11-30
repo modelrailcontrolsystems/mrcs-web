a5a53fe99610fb5ba70881a95923640d154775a0
Bruno Beloff <bruno.beloff@southcoastscience.com>
2025-11-30 18:01:19 +0000

    Added mrcs_fastapi CLI

---
+++ src/mrcs_web/app/routers/message_logger.py
@@ -0,0 +1,38 @@
+"""
+Created on 27 Nov 2025
+
+@author: Bruno Beloff (bbeloff@me.com)
+
+Message logger (MLG) API
+
+https://fastapi.tiangolo.com/tutorial/bigger-applications/#an-example-file-structure
+"""
+
+from fastapi import APIRouter
+
+from mrcs_core.data.json import JSONify
+from mrcs_core.operations.recorder.message_recorder import MessageRecorder
+from mrcs_core.sys.environment import Environment
+from mrcs_core.sys.logging import Logging
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+env = Environment.get()
+
+Logging.config(env.log_name, level=env.log_level)
+logger = Logging.getLogger()
+
+logger.info(f'message_logger: {env}')
+
+router = APIRouter()
+recorder = MessageRecorder.construct(env.ops_mode)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+@router.get("/mlg/latest", tags=["messages"])
+async def latest_messages(limit: int = 10):
+    records = list(recorder.find_latest(limit))
+
+    return JSONify.as_jdict(records)
---
+++ src/mrcs_web/cli/args/fastapi_args.py
@@ -0,0 +1,35 @@
+"""
+Created on 30 Nov 2025
+
+@author: Bruno Beloff (bbeloff@me.com)
+
+https://realpython.com/command-line-interfaces-python-argparse/
+"""
+
+from mrcs_core.cli.args.mrcs_args import MRCSArgs
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class FastAPIArgs(MRCSArgs):
+    """unix command line handler"""
+
+    def __init__(self, description):
+        super().__init__(description)
+
+        self._parser.add_argument("-r", "--reload", action="store_true", help='reload on changed source')
+
+        self._args = self._parser.parse_args()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def reload(self):
+        return self._args.reload
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return f'FastAPIArgs:{{test:{self.test}, reload:{self.reload}, indent:{self.indent}, verbose:{self.verbose}}}'
